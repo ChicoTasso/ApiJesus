f213f1e4f2958f14c46a398fcd3fd294844c51e0
ChicoTasso <tassomaisdogi@gmail.com>
2024-10-21 21:13:30 -0300

    correcaominima

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
-import Perfil from "./components/Perfil";
-import { Usuarios } from "./moks/Usuarios";
-import { Postagens } from './moks/Postagens';
-import Postagem from './components/Publicacao';
+import Perfil from "../components/Perfil";
+import { Usuarios } from "../moks/Usuarios";
+import { Postagens } from '../moks/Postagens';
+import Postagem from '../components/Publicacao';
 
 
 export default function HomeScreen() {
