05d552e84b2448aef16981314677af4fb7f8b8d3
willianjohny05 <willianjohny05@gmail.com>
2024-10-24 19:11:12 -0300

    Criando a tela de update post

---
+++ screens/UpdatePost.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Alert, Text } from 'react-native';
+
+export default function UpdatePost({ navigation }) {
+  const [titulo, setTitulo] = useState('');
+  const [texto, setTexto] = useState('');
+
+  const publicarPost = () => {
+    if (titulo === '' || texto === '') {
+      Alert.alert('Erro', 'Por favor, preencha todos os campos.');
+      return;
+    }
+    Alert.alert('Sucesso', 'Post publicado com sucesso!');
+    
+    navigation.navigate('Posts');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Título</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Título do Post"
+        placeholderTextColor="#000" 
+        value={titulo}
+        onChangeText={setTitulo}
+      />
+      <Text style={styles.label}>Conteúdo</Text> 
+      <TextInput
+        style={[styles.input, styles.textArea]}
+        placeholder="Escreva o conteúdo do post aqui..."
+        placeholderTextColor="#000" 
+        value={texto}
+        onChangeText={setTexto}
+        multiline
+        numberOfLines={4}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Publicar"
+          onPress={publicarPost}
+          color="#000" 
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#007BFF',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFFFFF', 
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#FFF', 
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 15,
+    fontSize: 18,
+    color: '#000', 
+    backgroundColor: '#FFF', 
+  },
+  textArea: {
+    height: 100,
+    textAlignVertical: 'top',
+  },
+  buttonContainer: {
+    backgroundColor: '#0056b3', 
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+});
